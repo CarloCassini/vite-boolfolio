9a4aa810fa11c37d36657ef480225caa28adbdd4
CarloCassini <c.cassini87@gmail.com>
2023-11-07 16:37:42 +0100

    inseriti i link nella header

--- src/components/AppHeade.vue
+++ src/components/AppHeade.vue
@@ -8,12 +8,20 @@ export default {
 </script>
 
 <template>
-  <h1 class="container my-3 debug">App header</h1>
-
-  proviamo a inserire una navbar che mi manda in giro per il progetto
   <nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-      <span class="navbar-brand mb-0 h1">HOME</span>
+    <div class="container">
+      <div class="d-flex g-3">
+        <div class="container-fluid">
+          <router-link :to="{ name: 'home' }">
+            <span class="navbar-brand mb-0 h1">HOME</span>
+          </router-link>
+        </div>
+        <div class="container-fluid">
+          <router-link :to="{ name: 'altro' }">
+            <span class="navbar-brand mb-0 h1">Altro</span>
+          </router-link>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
